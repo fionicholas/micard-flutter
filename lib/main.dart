88e44fa93c1eb6aedc2a085d48fefdbebdab3c27
fionicholas <fiorentcheaters@gmail.com>
2020-05-08 12:00:30 +0700

    feat(Main) : add card for email and phone number

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -32,6 +33,51 @@ class MainApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     fontFamily: 'Source Sans Pro'
                 ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                elevation: 10.0,
+                color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone_android,
+                        color: Colors.teal[900],
+                      ),
+                    title: Text(
+                      '+62 812 1234 5678',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal[900]
+                      ),
+                    )
+                  ),
+              ),
+              Card(
+                elevation: 10.0,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[900]
+                    ),
+                    title : Text(
+                      'fiorent@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal[900]
+                    ),
+                ),
+              )
               )
             ],
           )
@@ -40,4 +86,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
